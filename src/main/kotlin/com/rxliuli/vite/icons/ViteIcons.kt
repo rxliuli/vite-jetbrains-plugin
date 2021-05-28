3628bb52852b49014e4b795e940a5c5be4518295
rxliuli <rxliuli@gmail.com>
2021-05-28 14:28:26 +0800

    refactor: 移动 icons 的目录到根 resources/ 下

--- src/main/kotlin/com/rxliuli/vite/icons/ViteIcons.kt
+++
@@ -1,8 +0,0 @@
-package com.rxliuli.vite.icons
-
-import com.intellij.openapi.util.IconLoader
-
-object ViteIcons {
-    val ICON_16 = IconLoader.getIcon("/com/rxliuli/vite/icons/icon16.png")
-    val ICON_24 = IconLoader.getIcon("/com/rxliuli/vite/icons/icon24.png")
-}
