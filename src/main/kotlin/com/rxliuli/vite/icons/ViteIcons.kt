9742baf97215c2362b705457b2f2d990fe4142ad
rxliuli <rxliuli@gmail.com>
2021-05-28 12:49:17 +0800

    fix: 修复图标的路径错误

--- src/main/kotlin/com/rxliuli/vite/icons/ViteIcons.kt
+++ src/main/kotlin/com/rxliuli/vite/icons/ViteIcons.kt
@@ -3,6 +3,6 @@ package com.rxliuli.vite.icons
 import com.intellij.openapi.util.IconLoader
 
 object ViteIcons {
-    val ICON_16 = IconLoader.getIcon("/com/vite/icons/icon16.png")
-    val ICON_24 = IconLoader.getIcon("/com/vite/icons/icon24.png")
+    val ICON_16 = IconLoader.getIcon("/com/rxliuli/vite/icons/icon16.png")
+    val ICON_24 = IconLoader.getIcon("/com/rxliuli/vite/icons/icon24.png")
 }
