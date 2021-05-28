481d55791b0e2469259fbc19664514834fe29df7
rxliuli <rxliuli@gmail.com>
2021-05-28 11:46:20 +0800

    修复一些配置错误让插件能够正确运行

--- src/main/kotlin/com/vite/cli/ViteCliProjectGenerator.kt
+++ src/main/kotlin/com/vite/cli/ViteCliProjectGenerator.kt
@@ -1,136 +1,114 @@
 package com.vite.cli
 
 import com.intellij.execution.filters.Filter
-import com.intellij.execution.process.ProcessAdapter
-import com.intellij.execution.process.ProcessEvent
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.ide.util.projectWizard.SettingsStep
-import com.intellij.javascript.CreateRunConfigurationUtil
-import com.intellij.javascript.nodejs.packages.NodePackageUtil
-import com.intellij.javascript.nodejs.util.NodePackage
 import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
 import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
-import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.ProjectGeneratorPeer
-import com.intellij.ui.components.JBCheckBox
-import com.intellij.util.PathUtil
+import com.intellij.ui.components.JBList
+import com.intellij.util.ui.UIUtil
 import com.vite.icons.ViteIcons
 import java.io.File
-import java.io.IOException
-import java.nio.charset.StandardCharsets
-import javax.swing.Icon
-import javax.swing.JPanel
+import javax.swing.*
 
 class ViteCliProjectGenerator : NpmPackageProjectGenerator() {
-    private val LOG = Logger.getInstance(ViteCliProjectGenerator::class.java)
-
-    private val DEFAULT_PROJECT_SETUP_INITIALLY_SELECTED = true
-    private val DEFAULT_PROJECT_SETUP_KEY = Key.create<Boolean>("vue.project.generator.default.setup")
-
-    private val PACKAGE_NAME = "@vue/cli"
-    private val VUE_EXECUTABLE = "vue"
-    private val CREATE_COMMAND = "create"
+    private val PackageName = "@vitejs/create-app"
 
     override fun getName(): String {
-        return "vite.project.generator.name"
+        return "Vite CLI"
     }
 
     override fun getDescription(): String {
-        return "vue.project.generator.description"
+        return "创建一个 Vite 项目"
     }
 
     override fun getIcon(): Icon {
         return ViteIcons.ICON_16
     }
 
-    override fun createPeer(): ProjectGeneratorPeer<Settings> {
-        val defaultSetupCheckbox = JBCheckBox(
-            "vue.project.generator.use.default.project.setup",
-            DEFAULT_PROJECT_SETUP_INITIALLY_SELECTED
-        )
-        return object : NpmPackageGeneratorPeer() {
-            override fun createPanel(): JPanel {
-                val panel = super.createPanel()
-                panel.add(defaultSetupCheckbox)
-                return panel
-            }
-
-            override fun buildUI(settingsStep: SettingsStep) {
-                super.buildUI(settingsStep)
-                settingsStep.addSettingsComponent(defaultSetupCheckbox)
-            }
-
-            override fun getSettings(): Settings {
-                val settings = super.getSettings()
-                settings.putUserData(DEFAULT_PROJECT_SETUP_KEY, defaultSetupCheckbox.isSelected)
-                return settings
-            }
-        }
-    }
-
     override fun customizeModule(baseDir: VirtualFile, entry: ContentEntry) {}
-
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> {
-        val default = settings?.getUserData(DEFAULT_PROJECT_SETUP_KEY) ?: DEFAULT_PROJECT_SETUP_INITIALLY_SELECTED
-        return if (default) arrayOf(CREATE_COMMAND, "--default", ".") else arrayOf(CREATE_COMMAND, ".")
-    }
-
-    override fun generatorArgs(project: Project, baseDir: VirtualFile): Array<String> {
-        return emptyArray()
-    }
 
     override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> {
         return emptyArray()
     }
 
-    override fun executable(pkg: NodePackage): String {
-        return pkg.systemDependentPath + File.separator + "bin" + File.separator + "vue.js"
-    }
-
     override fun packageName(): String {
-        return PACKAGE_NAME
+        return PackageName
     }
 
     override fun presentablePackageName(): String {
-        return "vue.project.generator.presentable.package.name"
+        return "Vite CLI"
     }
 
     override fun getNpxCommands(): List<NpxPackageDescriptor.NpxCommand> {
-        return listOf(NpxPackageDescriptor.NpxCommand(PACKAGE_NAME, VUE_EXECUTABLE))
+        return listOf(NpxPackageDescriptor.NpxCommand(PackageName, "create-app"))
     }
 
-    override fun validateProjectPath(path: String): String? {
-        val error = NodePackageUtil.validateNpmPackageName(PathUtil.getFileName(path))
-        return error ?: super.validateProjectPath(path)
+    final val SettingsTemplateKey = Key.create<String>("template")
+
+    override fun createPeer(): ProjectGeneratorPeer<Settings> {
+        val pluginNameTextField = JBList(
+            "vanilla",
+            "vanilla-ts",
+            "vue",
+            "vue-ts",
+            "react",
+            "react-ts",
+            "preact",
+            "preact-ts",
+            "lit-element",
+            "lit-element-ts",
+            "svelte",
+            "svelte-ts",
+        )
+
+        return object : NpmPackageGeneratorPeer() {
+            override fun buildUI(settingsStep: SettingsStep) {
+                super.buildUI(settingsStep)
+                pluginNameTextField.setSelectedValue("react-ts", true)
+                settingsStep.addSettingsField(
+                    UIUtil.replaceMnemonicAmpersand(
+                        "请选择一个模板"
+                    ),
+                    pluginNameTextField
+                )
+            }
+
+            override fun getSettings(): Settings {
+                val settings = super.getSettings()
+                settings.putUserData(
+                    SettingsTemplateKey,
+                    pluginNameTextField.selectedValue
+                )
+                return settings
+            }
+        }
+    }
+
+    override fun generatorArgs(project: Project, baseDir: VirtualFile, settings: Settings): Array<String> {
+        val template = settings.getUserData<String>(SettingsTemplateKey)
+        return arrayOf(" ${baseDir.name} --template $template")
+    }
+
+    override fun generateProject(project: Project, baseDir: VirtualFile, settings: Settings, module: Module) {
+//        baseDir.delete(project) // TODO 此处无法真正清理
+        super.generateProject(project, baseDir, settings, module)
     }
 
     override fun onProcessHandlerCreated(processHandler: ProcessHandler) {
-        processHandler.addProcessListener(object : ProcessAdapter() {
-            override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
-                // https://github.com/vuejs/vue-cli/blob/dev/packages/%40vue/cli/lib/create.js#L43
-                if (event.text.contains("Generate project in current directory?")) {
-                    event.processHandler.removeProcessListener(this)
-                    val processInput = event.processHandler.processInput
-                    if (processInput != null) {
-                        try {
-                            processInput.write("yes\n".toByteArray(StandardCharsets.UTF_8))
-                            processInput.flush()
-                        } catch (e: IOException) {
-                            LOG.warn("Failed to write 'yes' to the Vue CLI console.", e)
-                        }
-                    }
-                }
-            }
-        })
+
+        super.onProcessHandlerCreated(processHandler)
     }
 
-    override fun onGettingSmartAfterProjectGeneration(project: Project, baseDir: VirtualFile) {
-        super.onGettingSmartAfterProjectGeneration(project, baseDir)
-        CreateRunConfigurationUtil.debugConfiguration(project, 8080)
-        CreateRunConfigurationUtil.npmConfiguration(project, "serve")
+    override fun workingDir(settings: Settings?, baseDir: VirtualFile): File {
+        return super.workingDir(settings, baseDir).parentFile
     }
 }
